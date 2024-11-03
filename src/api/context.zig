1c6236e53f3f85b9bf61d32523c089ce4ade75f8
Matthew Russo <matthew@matthewclayrusso.com>
2024-11-03 13:06:44 -0800

    Context : make Context concrete

--- src/api/context.zig
+++ src/api/context.zig
@@ -1,44 +1,41 @@
+const std = @import("std");
+
 pub const ContextValue = struct {};
 
 pub const Context = struct {
     const Self = @This();
 
-    ptr: *anyopaque,
+    allocator: std.mem.Allocator,
+    values: std.StringHashMap(ContextValue),
 
-    getValueFn: *const fn (*anyopaque, []const u8) ?ContextValue,
-    withValueFn: *const fn (*anyopaque, []const u8, ContextValue) *Self,
-
-    pub fn init(ptr: anytype) Self {
-        const Ptr = @TypeOf(ptr);
-        const ptr_info = @typeInfo(Ptr);
-
-        if (ptr_info != .pointer) @compileError("ptr must be a pointer");
-        if (ptr_info.pointer.size != .One) @compileError("ptr must be a single item pointer");
-
-        const gen = struct {
-            pub fn getValueImpl(pointer: *anyopaque, name: []const u8) ?ContextValue {
-                const self: Ptr = @ptrCast(@alignCast(pointer));
-                return @call(.always_inline, ptr_info.pointer.child.getValue, .{ self, name });
-            }
-
-            pub fn withValueImpl(pointer: *anyopaque, name: []const u8, value: ContextValue) *Self {
-                const self: Ptr = @ptrCast(@alignCast(pointer));
-                return @call(.always_inline, ptr_info.pointer.child.withValue, .{ self, name, value });
-            }
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .allocator = allocator,
+            .values = std.StringHashMap(ContextValue).init(allocator),
         };
+    }
 
-        return .{
-            .ptr = ptr,
-            .getValueFn = gen.getValueImpl,
-            .withValueFn = gen.withValueImpl,
+    pub fn clone(self: *Self) Self {
+        return Self{
+            .allocator = self.allocator,
+            .values = self.values.clone(),
         };
     }
 
     pub fn getValue(self: *Self, name: []const u8) ?ContextValue {
-        return self.getValueFn(self.ptr, name);
+        return self.values.get(name);
     }
 
-    pub fn withValue(self: *Self, name: []const u8, value: ContextValue) *Self {
-        return self.withValueFn(self.ptr, name, value);
+    pub fn withValue(self: *Self, name: []const u8, value: ContextValue) Self {
+        var new_context = self.clone();
+        new_context.setValue(name, value);
+        return new_context;
+    }
+
+    // internal mutator, not publicly accessible. users will generate updated
+    // contexts via `withValue` which clones the current Context
+    fn setValue(self: *Self, name: []const u8, value: ContextValue) void {
+        // ignore OOMs
+        self.values.put(name, value) catch unreachable;
     }
 };
